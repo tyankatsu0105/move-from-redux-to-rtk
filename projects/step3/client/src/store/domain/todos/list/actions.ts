f87789b830103a0157cba1bdc6322dd51ee8b487
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-04-29 23:09:01 +0900

    use createAction, createReducer

--- projects/step3/client/src/store/domain/todos/list/actions.ts
+++ projects/step3/client/src/store/domain/todos/list/actions.ts
@@ -1,42 +1,21 @@
+import * as ReduxToolkit from "@reduxjs/toolkit";
+
 import * as Types from "./types";
 import * as Entity from "../../../../application/domain/todos/entity";
 
-export type Actions =
-  | ReturnType<typeof fetch>
-  | ReturnType<typeof create>
-  | ReturnType<typeof update>
-  | ReturnType<typeof remove>;
-
-export const fetch = (payload: { todos?: Entity.Todo[] | null }) => ({
-  type: Types.FETCH,
-  payload,
-});
-
-export const create = (
-  payload: { createTodo: { todo?: Entity.Todo | null } } | null
-) => ({
-  type: Types.CREATE,
-  payload,
-});
-
-export const update = (
-  payload: {
-    updateTodo: {
-      todo?: Pick<Entity.Todo, "updatedAt" | "isDone" | "id"> | null;
-    };
-  } | null
-) => ({
-  type: Types.UPDATE,
-  payload,
-});
-
-export const remove = (
-  payload: {
-    removeTodo: {
-      todo?: Pick<Entity.Todo, "id"> | null;
-    };
-  } | null
-) => ({
-  type: Types.REMOVE,
-  payload,
-});
+export const fetch = ReduxToolkit.createAction<{
+  todos?: Entity.Todo[] | null;
+}>(Types.FETCH);
+export const create = ReduxToolkit.createAction<{
+  createTodo: { todo?: Entity.Todo | null };
+} | null>(Types.CREATE);
+export const update = ReduxToolkit.createAction<{
+  updateTodo: {
+    todo?: Pick<Entity.Todo, "updatedAt" | "isDone" | "id"> | null;
+  };
+} | null>(Types.UPDATE);
+export const remove = ReduxToolkit.createAction<{
+  removeTodo: {
+    todo?: Pick<Entity.Todo, "id"> | null;
+  };
+} | null>(Types.REMOVE);
