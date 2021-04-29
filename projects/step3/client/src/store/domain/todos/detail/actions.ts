401636e7627b69cb7df13ed97a772c8eadc097f6
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-04-29 23:15:30 +0900

    detail replace

--- projects/step3/client/src/store/domain/todos/detail/actions.ts
+++ projects/step3/client/src/store/domain/todos/detail/actions.ts
@@ -1,9 +1,8 @@
+import * as ReduxToolkit from "@reduxjs/toolkit";
+
 import * as Types from "./types";
 import * as Entity from "../../../../application/domain/todos/entity";
 
-export type Actions = ReturnType<typeof fetch>;
-
-export const fetch = (payload: { todo?: Entity.Todo | null }) => ({
-  type: Types.FETCH,
-  payload,
-});
+export const fetch = ReduxToolkit.createAction<{ todo?: Entity.Todo | null }>(
+  Types.FETCH
+);
--- projects/step3/client/src/store/domain/todos/detail/reducer.ts
+++ projects/step3/client/src/store/domain/todos/detail/reducer.ts
@@ -1,10 +1,9 @@
-import produce from "immer";
+import * as ReduxToolkit from "@reduxjs/toolkit";
 
 import * as DTO from "./dto";
 
-import * as Types from "./types";
 import * as Status from "../../../status";
-import { Actions } from "./actions";
+import * as Actions from "./actions";
 
 import * as Entity from "../../../../application/domain/todos/entity";
 
@@ -25,26 +24,42 @@ const initialState: State = {
   createdAt: "",
   updatedAt: "",
 };
+export const reducer = ReduxToolkit.createReducer(initialState, (builder) => {
+  builder.addCase(Actions.fetch, (state, action) => {
+    const payload = DTO.Fetch.toEntity(action.payload);
+    if (payload == null) return;
 
-export const reducer = (state = initialState, action: Actions) => {
-  switch (action.type) {
-    case Types.FETCH:
-      return produce(state, (draft) => {
-        const payload = DTO.Fetch.toEntity(action.payload);
-        if (payload == null) return;
+    state.status = Status.status.SUBMITTING;
 
-        draft.status = Status.status.SUBMITTING;
+    state.id = payload.id;
+    state.description = payload.description;
+    state.isDone = payload.isDone;
+    state.createdAt = payload.createdAt;
+    state.updatedAt = payload.updatedAt;
 
-        draft.id = payload.id;
-        draft.description = payload.description;
-        draft.isDone = payload.isDone;
-        draft.createdAt = payload.createdAt;
-        draft.updatedAt = payload.updatedAt;
+    state.status = Status.status.SUCCESS;
+  });
+});
 
-        draft.status = Status.status.SUCCESS;
-      });
+// export const reducer = (state = initialState, action: Actions) => {
+//   switch (action.type) {
+//     case Types.FETCH:
+//       return produce(state, (draft) => {
+//         const payload = DTO.Fetch.toEntity(action.payload);
+//         if (payload == null) return;
 
-    default:
-      return initialState;
-  }
-};
+//         draft.status = Status.status.SUBMITTING;
+
+//         draft.id = payload.id;
+//         draft.description = payload.description;
+//         draft.isDone = payload.isDone;
+//         draft.createdAt = payload.createdAt;
+//         draft.updatedAt = payload.updatedAt;
+
+//         draft.status = Status.status.SUCCESS;
+//       });
+
+//     default:
+//       return initialState;
+//   }
+// };
