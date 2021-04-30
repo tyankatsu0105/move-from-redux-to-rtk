f0edc106d24b318d26823974ee9bc374cb8cb468
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-05-01 01:17:47 +0900

    step8 remove

--- projects/step8/client/src/store/index.ts
+++ projects/step8/client/src/store/index.ts
@@ -19,12 +19,6 @@ export const createStore = () => {
 
 export type RootState = ReturnType<ReturnType<typeof createStore>["getState"]>;
 
-export type ThunkAction<R> = (
-  dispatch: ReturnType<typeof createStore>["dispatch"],
-  getState: ReturnType<typeof createStore>["getState"],
-  extraArgument: typeof Middleware.extraArgument
-) => R;
-
 /**
  * Override type AsyncThunkConfig of ReduxToolkit
  * @see https://github.com/reduxjs/redux-toolkit/blob/de1282c1ec7eb8db1590a60c55f0ec21004b8675/src/createAsyncThunk.ts#L65-L70
